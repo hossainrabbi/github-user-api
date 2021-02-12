64b1019027258b47f1d1f2dea31ebcc5f7ad5a75
Hossain <hossainrabbi259@gmail.com>
2021-02-12 15:16:05 +0600

    add username and img

--- src/script.js
+++ src/script.js
@@ -1,10 +1,19 @@
-"use strict";
-const username = 'hossainrabbi';
-const API = `https://api.github.com/users/${username}`;
-fetch(API)
+'use strict';
+const userName = document.getElementById('user-name');
+const userImg = document.getElementById('user-img');
+
+const clickSearchBtn = () => {
+  const inputUser = document.getElementById('search-input');
+  const API = `https://api.github.com/users/${inputUser.value}`;
+  fetch(API)
     .then((res) => res.json())
     .then((data) => dataFunction(data));
+};
+
 const dataFunction = (data) => {
-    const userName = document.getElementById('user-name');
-    console.log(data.login);
+  userImg.src = data.avatar_url;
+  userImg.alt = data.login;
+  userName.innerText = `@${data.login}`;
+  userName.href = data.html_url;
+  console.log(data);
 };
